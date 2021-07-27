4c71b8ca4e4105e43f45b88a16fd09efff4005d1
Adrian Dunham <adunham95@gmail.com>
2021-07-27 08:20:55 -0400

    Added react router

--- src/App.js
+++ src/App.js
@@ -1,10 +1,28 @@
 import Header from './components/header/header';
-import logo from './logo.svg';
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Link
+} from "react-router-dom";
 
 function App() {
   return (
     <div className="App">
+      <Router>
       <Header/>
+      <Switch>
+          <Route path="/about">
+            {/* <About /> */}
+          </Route>
+          <Route path="/users">
+            {/* <Users /> */}
+          </Route>
+          <Route path="/">
+            {/* <Home /> */}
+          </Route>
+        </Switch>
+      </Router>
     </div>
   );
 }
